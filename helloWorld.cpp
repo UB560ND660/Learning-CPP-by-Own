420799b81405eb62d1e8eae23e1ef138fe863208
Joy <mondolj21@gmail.com>
2025-10-06 02:30:41 +0600

    com

---
+++ helloWorld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+    int x;
+    int y;
+    x = 5;
+    y = 6;
+
+    std::cout << "x is " << x << std::endl;
+    std::cout << "y is " << y << std:: endl;
+
+    return 0;
+}
