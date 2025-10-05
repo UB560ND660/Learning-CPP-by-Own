5bf1e3ac81f0356a3714c225335fd77266d2c8ce
Joy <mondolj21@gmail.com>
2025-10-06 02:45:33 +0600

    commit

--- program2.cpp
+++ program2.cpp
@@ -3,7 +3,6 @@
 int main() {
 
     // integer (Whole number)
-
     int age = 21;
     int year = 2024;
     int days = 7.5;
@@ -18,9 +17,24 @@ int main() {
     char grade = 'A';
     char initial = 'B';
 
+    // Boolean(true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = false;
+
+    // String (Object that represents a squence of text)
+    std::string name = "Joy";
+    std::string day = "Sunday";
+    std::string food = "pizza";
+    std::string adress = "Ishow Speed Burger King";
+
+
+    // displaying 
     std::cout << days << std::endl;
     std::cout << price << std::endl;
     std::cout << initial << std::endl;
+    std::cout << name << std::endl;
+    std::cout << "Hello " << name << " go to this adress -> " << adress << "<- to get free burgers " << std::endl;
 
     return 0;
 }
