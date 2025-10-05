d209bb6dd106b36832d7b62dbe1f47199b63edf3
Joy <mondolj21@gmail.com>
2025-10-06 03:10:16 +0600

    day1

---
+++ ConstVariable.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+    // The const keyword make sures that variable's value is constant
+    // tells the compiler to prevent anything from modifying it
+    // (read only)
+
+    const double pi = 3.1416;
+    pi = 420.69;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
+
+    std::cout << circumference << " cm" << std::endl;
+}
