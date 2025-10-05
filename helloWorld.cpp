72750899882695cda54cd8a372bc1a796db269a5
Joy <mondolj21@gmail.com>
2025-10-06 02:38:17 +0600

    git

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -2,12 +2,13 @@
 
 int main() {
     int x;
-    int y;
+    int y;  
     x = 5;
     y = 6;
-
+    int sum = x + y;
     std::cout << "x is " << x << std::endl;
     std::cout << "y is " << y << std:: endl;
+    std::cout << "Sum " << sum << std:: endl;
 
     return 0;
 }
---
+++ program2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main() {
+
+    // integer (Whole number)
+
+    int age = 21;
+    int year = 2024;
+    int days = 7.5;
+
+
+    // Double (Number including Decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temp = 25.1; 
+    
+    // Char (Single character)
+    char grade = 'A';
+    char initial = 'B';
+
+    std::cout << days << std::endl;
+    std::cout << price << std::endl;
+    std::cout << initial << std::endl;
+
+    return 0;
+}
