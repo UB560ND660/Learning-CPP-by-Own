6093431c5f828d71d2d2bbf7a1d79ff838209573
Joy <mondolj21@gmail.com>
2025-10-06 18:48:48 +0600

    com

--- UserInput.cpp
+++ UserInput.cpp
@@ -9,11 +9,13 @@ int main()
     std::string name;
     int age;
 
-    std::cout << "what's your full name? : ";
-    std::getline(std::cin, name);
-
     std::cout << "what's your age? : ";
     std::cin >> age;
+    
+    // ws => white space, after input in integer it leaves a blank space which directed into next string input, then it skips the string variable input
+    std::cout << "what's your full name? : ";
+    std::getline(std::cin >> std::ws, name);
+
 
     std::cout << "Hello" << name << "\n";
     std::cout << "You are " << age << " years old " << std::endl;
