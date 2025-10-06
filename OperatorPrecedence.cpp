6ce6d0fe9cb5f9686572f2b545057d3e95e3b5c6
Joy <mondolj21@gmail.com>
2025-10-06 15:06:09 +0600

    pusing codes

--- OperatorPrecedence.cpp
+++ OperatorPrecedence.cpp
@@ -6,5 +6,10 @@ int main() {
     // Multiplication & Divison
     // Addition and Substraction
 
+    // int number = 6 - 5 + 4 * 3 / 2; => 7
+    int number = 6 - (5 + 4) * 3 / 2;
+
+    std::cout << number << std::endl;
+
     return 0;
 }
---
+++ typeConversion.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main() {
+    // there are two types of type conversion 
+    // 1. implicit = automatic 
+    // 2. explicit = manually 
+    
+    // double x = (int) 3.1416;
+    char x = 100;
+ 
+    std::cout << x;
+
+    return 0;
+}
