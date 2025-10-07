fa2eeb06a25ff5cfcb017068a8e0e2424e6cc496
Joy <mondolj21@gmail.com>
2025-10-07 08:29:02 +0600

    com

---
+++ MathFunction.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+int main() {
+
+    double x = 2;
+    double y = 4;
+    double z;
+
+    // z = std::max(x,y);
+    // z = std::min(x,y);
+    // z = pow (x,y);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // z = round (3.14);
+    // z = ceil(3.14);
+    z = floor(4.9);
+
+
+    std::cout << z <<std::endl;
+
+    return 0;
+}
